363a806ff443d7b5fc375f9bd9d05feb9e8ab631
Ayaz Ali <ayaz.ujjan@yahoo.com>
2018-07-16 16:58:38 +0200

    components has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponant} from './server/server.componant';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponant,
+    ServersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/server/server.componant.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.componant.html',
+  styleUrls: ['./server.componant.css']
+})
+
+export class ServerComponant {
+
+}
